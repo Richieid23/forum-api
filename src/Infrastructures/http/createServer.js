1db9f6676aea99d9ced707ad4b915f4908d53561
Richieid23 <fitrarizki23@gmail.com>
2022-09-08 20:06:49 +0700

    pass ping test case

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -62,6 +62,14 @@ const createServer = async (container) => {
     }),
   });
 
+  server.route({
+    method: 'GET',
+    path: '/ping',
+    handler: () => ({
+      value: 'Pong!',
+    }),
+  });
+
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
