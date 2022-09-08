8223c1e4bcfc4193ffa9aa219dd8b2eccc8254a6
Richieid23 <fitrarizki23@gmail.com>
2022-09-08 20:02:42 +0700

    add ping test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -54,3 +54,19 @@ describe('when GET /', () => {
     expect(responseJson.value).toEqual('Hello world!');
   });
 });
+
+describe('when GET /ping', () => {
+  it('should return 200 and hello world', async () => {
+    // Arrange
+    const server = await createServer({});
+    // Action
+    const response = await server.inject({
+      method: 'GET',
+      url: '/ping',
+    });
+    // Assert
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(200);
+    expect(responseJson.value).toEqual('Pong!');
+  });
+});
